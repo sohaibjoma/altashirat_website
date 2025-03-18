0f7659491d9a06e1ce44ab7f21ff14a623aa8830
Neveen Abbas <neveen.abbas555@gmail.com>
2025-03-19 00:27:34 +0200

    stuff

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
+  <!-- <div>
     <h1>Profile Page</h1>
     <div v-if="user">
       <p>Name: {{ user.firstname }}</p>
       <p>Email: {{ user.email }}</p>
     </div>
     <div v-else></div>
-  </div>
+  </div> -->
+  <v-container>
+    <v-card variant="outlined" class="w-75 rounded-lg custom-card pt-5">
+      <div class="d-flex justify-start align-center">
+      <div class="profile__h--starter me-5"></div>
+        <Image
+          name="profile-form-icon.png"
+          alt="profile tile image"
+          width="30"
+        />
+        
+        <h3 class="text-text profile__h ps-3">المعلومات الشخصية الخاصة بك</h3>
+      </div>
+      <!-- form start -->
+      <VeeForm v-slot="{ handleSubmit }">
+        <v-form @submit.prevent="handleSubmit(formSubmitting)">
+          
+        </v-form>
+      </VeeForm>
+    </v-card>
+  </v-container>
 </template>
 
 <script setup>
+definePageMeta({
+  layout: "profile",
+});
+
 const authStore = useAuthStore();
 
 onMounted(() => {
@@ -18,3 +42,9 @@ onMounted(() => {
 
 const user = computed(() => authStore.user);
 </script>
+
+<style>
+.custom-card {
+  border: 2px solid #d1d1d180;
+}
+</style>
