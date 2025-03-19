085722336489e5ae6f189b0adb516ba671fc5ce5
sohaibjoma <sohaibjoma1@gmail.com>
2025-03-19 21:23:20 +0200

    Made privacy and policy page

---
+++ components/shared/Chips/MainChip/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-chip class="main-chip" :elevation="elevation">
+    <slot></slot>
+  </v-chip>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  elevation: {
+    type: Number,
+    default: 2,
+  },
+});
+</script>
+
+<style scoped>
+.main-chip {
+  font-weight: 500;
+  padding: 24px 24px;
+  background: var(--gradient-button);
+  color: white;
+}
+</style>
---
+++ pages/privacy-policy/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <Image
+    name="privacy-policy.png"
+    class="d-block mx-auto mt-12"
+    width="100%"
+    height="auto"
+  />
+  <div class="position-relative">
+    <MainChip class="chip-position">{{ $t("privacyPolicy.title") }}</MainChip>
+  </div>
+
+  <v-container class="d-flex justify-center mt-12">
+    <h2>سياسة الخصوصية</h2>
+  </v-container>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.chip-position {
+  position: absolute;
+  top: -25px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
